45c8b0abd3f58fff691fc1eae5d6dacc2481ca5a
AlexGithub3427 <altr3427@gmail.com>
2025-09-07 10:08:35 -0700

    Final

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-# Thesis: 
-# Women will have a higher survival rate than men. Youth will have a higher survival rate than adults.
-# Rich individuals will have a higher survival rate than poor. 
-
 
 df = pd.read_csv('Titanic-Dataset.csv.xls')
 
@@ -68,15 +64,3 @@ axes[1, 3].set_title('Survival Rate by Embarked Location')
 
 plt.show()
 
-# Conclusion:
-# Women had significantly greater survival rates. Based on ticket class and fare cost, 
-# wealth correlated with greater survival as well. Adult ages (20 - 40) showed significantly
-# worse survival rates than younger and older ages. Number of parents and children showed an
-# inconclusive relation to survival rate. Number of siblings/spouses did show a trend (greatest
-# survival at 1 with linear decrease to 4). The Titles graph further emphasized trends we have 
-# established as we see Mrs, Miss, and Master (referring to adult women, girls, and boys) having
-# exceptional survival rates, while Mr (adult men) having drastically lower survival rates. Rare
-# titles surprising did not signficantly correlate with a great survival rate as it neared 35% survival.
-# Finally, those embarking from the port of Cherbourg(C) had a significantly greater survival rate
-# than those embarking from the port of Queenstown(Q) and Southhampton(S). Thus, alongside other
-# findings, our thesis is correct.
